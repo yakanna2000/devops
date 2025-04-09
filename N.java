1f2bf67c65c428144b68a7c4bc9cd24afb422201
yakanna <yakannakadem@gmail.com>
2025-04-09 14:58:19 +0530

    firts commit

---
+++ N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class N{
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/yakanna2000/devops.git
+// git push -u origin main
