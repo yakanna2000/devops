fb53bfbb88074b0cc7130fdb229c63da146a9d51
yakanna <yakannakadem@gmail.com>
2025-04-09 15:18:39 +0530

    commands added2

--- N.java
+++ N.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class N{
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("hello world 2");
     }
 }
 
