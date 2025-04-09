642e49556a6f10b1db59e8d953e92d6e4cad321d
yakanna <yakannakadem@gmail.com>
2025-04-09 15:16:57 +0530

    commands added

--- N.java
+++ N.java
@@ -12,3 +12,9 @@ public class N{
 // git branch -M main
 // git remote add origin https://github.com/yakanna2000/devops.git
 // git push -u origin main
+
+
+//git branch branchName    ---creates a branch with name branchName
+//git switch branchName    ---switches to the branch named branchName
+// git log  --- shows all commits
+//
