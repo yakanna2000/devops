2a53b800bd5e1052a8227e374ee8a15aa4db9d16
yakanna <yakannakadem@gmail.com>
2025-04-21 22:03:23 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello{
+    public static void main(String args[]){
+        System.out.println("hi");
+    }
+}
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/yakanna2000/devops.git
+// git push -u origin main
